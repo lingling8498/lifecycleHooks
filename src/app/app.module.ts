2b9c01f30486a6fda4facc6442adb4acf388f56e
lingling8498 <Ling.Wang@WolfDen.wolfmedical.com>
2018-09-06 14:43:50 -0700

    lifecycleHooks - Angular IO Tutorial Guide - May 21 2018

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+import { PeekABooParentComponent } from './peek-a-boo-parent.component';
+import { PeekABooComponent } from './peek-a-boo.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeekABooParentComponent,
+    PeekABooComponent
+    
   ],
   imports: [
     BrowserModule
